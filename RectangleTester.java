557e172ccad32e76f6d75f6943c92c7c65aebdcc
smithb <smithb@wlwv.k12.or.us>
2022-10-02 11:15:04 -0700

    initial commit

---
+++ RectangleTester.java
@@ -0,0 +1,21 @@
+public class RectangleTester 
+{
+    public static void main(String[] args)
+    {
+        /*
+         * Rectangle is the name of the class. Every Rectangle
+         * has a width and a height. But the specific instances
+         * have their own dimensions.
+         */
+        
+        //Make a rectangle called r1 with a width of 10 and height of 2 then print it
+        
+         
+         //print just the area of the rectangle
+         
+         
+         // Rectangle is the class and also the type of this object.
+         // Make a new rectangle called r2 with a width of 5 and height of 15, print it, then print just its area
+         
+    }
+}
